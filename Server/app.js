139e4e562984d1011838ab7c2792b292d5bacd1e
Patrick Cleary <vampiirecodes@gmail.com>
2017-05-22 03:44:34 -0400

    created the foundation of the server to enable websocket based multiplayer

---
+++ Server/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Vampiire on 5/22/17.
+ */
+
+
+const express = require('express'), app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, function(){
+    console.log(`listening on port ${port}`);
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('assets'));
+
+const gameStateController = require('./controllers/gameStateController');
---
+++ Server/assets/Score.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Vampiire on 5/22/17.
+ */
+
+function Score(){
+
+// determines if a point has been scored by the top or bottom player
+// awards point and updates respective DOM element
+    this.scorePoint = function(ball){
+        if(ball.y >= STAGE_HEIGHT){
+            topScore++;
+            this.showScore('topScore', topScore);
+            this.reset(ball);
+        }
+
+        if(ball.y <= 0){
+            bottomScore++;
+            this.showScore('bottomScore', bottomScore);
+            this.reset(ball);
+        }
+
+    };
+
+// on point score of a ball have that ball reset to center
+    this.reset = function(ball){
+        ball.y = width/2;
+        ball.x = height/2;
+        ball.xvel = 0;
+        ball.yvel = 0;
+
+    // issue with multiple balls: allows "ball drop" before all balls have been reset
+        // works fine with 1 ball
+        start = false;
+
+    };
+
+    this.showScore = function(pointsDiv, playerPoints){
+        document.getElementById(pointsDiv).textContent = playerPoints;
+    }
+}
