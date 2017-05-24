84be2afb93a7230eb73f9565a0ac54a367906943
Raintracer <rts@rtstevenson.com>
2017-05-23 20:06:34 -0400

    Experimental hitTesting between balls (And paddles to some extent). Balls react to each other, reflect, and transfer momentum.

--- Ball.js
+++ Ball.js
@@ -2,12 +2,12 @@
  * Created by Richard Tyler on 5/21/2017.
  */
 
-function Ball(x,y){
+function Ball(id,x,y){
 
-    GameObject.call(this,x,y);
+    GameObject.call(this,id,x,y);
 
 // ball diameter
-    const BallSize = 25;
+    const BallSize = 10;
     this.w = BallSize;
     this.h = BallSize;
 
@@ -18,14 +18,172 @@ function Ball(x,y){
         this.yvel = yvel;
     };
 
-
 // negates GameObject dynamic friction
     this.friction = 1;
+    this.staticFriction = 0;
 
 // draws the ball object
     this.show = function(){
         fill(255);
         ellipse(this.x,this.y,BallSize,BallSize);
+    };
+
+    // moves the game object in the x direction, assigns the old x position to the previous x position before movement
+// sets bounds in the x direction to contain the game object within the canvas
+    this.moveX = function(){
+
+        this.x += this.xvel;
+
+        // DETECT COLLISION WITH OTHER OBJECTS
+        for (let i = 0; i < GameObjects.length; i++) {
+
+            // If the target object and the current object aren't the same
+            if (GameObjects[i].id !== this.id) {
+                // If the ball is moving right
+                if (this.xvel > 0) {
+
+                    // If there is a collision on the right
+                    if (this.collidesRight(GameObjects[i]) || this.collidesTopRight(GameObjects[i]) || this.collidesBottomRight(GameObjects[i])) {
+
+                        // Set the active object against the left side of the target object
+                        this.setPosition(GameObjects[i].leftEdge() - this.w / 2, this.y);
+
+                        // Transfer Momentum
+                        this.TransferMomentumX(GameObjects[i]);
+
+                        // If the active object and the target object are moving in opposite, reflect the target object
+                        if(this.xDir() === -GameObjects[i].xDir()){
+
+                            GameObjects[i].reflectX();
+
+                        }
+
+                        // Reflect the active objects
+                        this.reflectX();
+
+                        console.log("Right Collision");
+                    }
+
+                }
+                else if (this.xvel < 0) {
+
+                    // If there is a collision on the left
+                    if (this.collidesLeft(GameObjects[i]) || this.collidesTopLeft(GameObjects[i]) || this.collidesBottomLeft(GameObjects[i])) {
+
+                        // Set the active object against the left side of the target object
+                        this.setPosition(GameObjects[i].rightEdge() + this.w / 2, this.y);
+
+                        // Transfer Momentum
+                        this.TransferMomentumX(GameObjects[i]);
+
+                        // If the active object and the target object are moving in opposite, reflect the target object
+                        if(this.xDir() === -GameObjects[i].xDir()){
+
+                            GameObjects[i].reflectX();
+
+                        }
+
+                        // Reflect the active objects
+                        this.reflectX();
+
+                        console.log("Left Collision");
+                    }
+
+                }
+            }
+        }
+
+        // DETECT COLLISION BOUNDARIES
+        // left edge boundary crossing prevention
+        if (this.x - (this.w / 2) < 0) {
+            this.x = this.w / 2;
+            this.reflectX();
+        }
+
+        // right edge boundary crossing prevention
+        else if (this.x + (this.w / 2) > width) {
+            this.x = width - (this.w / 2);
+            this.reflectX();
+        }
+
+    };
+
+    // moves the game object in the y direction, assigns the old y position to the previous y position before movement
+    // sets bounds in the y direction to contain the game object within the canvas
+    this.moveY = function(){
+
+        this.y += this.yvel;
+
+        // DETECT COLLISION WITH OTHER OBJECTS
+        for (let i = 0; i < GameObjects.length; i++) {
+
+            // If the target object and the current object aren't the same
+            if (GameObjects[i].id !== this.id) {
+                // If the ball is moving down
+                if (this.yvel > 0) {
+
+                    // If there is a collision on the bottom
+                    if (this.collidesBottomLeft(GameObjects[i]) || this.collidesBottom(GameObjects[i]) || this.collidesBottomRight(GameObjects[i])) {
+
+                        this.setPosition(this.x, GameObjects[i].topEdge() - this.h / 2);
+
+                        // Transfer Momentum
+                        this.TransferMomentumY(GameObjects[i]);
+
+                        // If the active object and the target object are moving in opposite, reflect the target object
+                        if(this.yDir() === -GameObjects[i].yDir()){
+
+                            GameObjects[i].reflectY();
+
+                        }
+
+                        // Reflect the active objects
+                        this.reflectY();
+
+                        console.log("Bottom Collision");
+                    }
+
+                }
+                // If the ball is moving up
+                else if (this.yvel < 0) {
+
+                    // If there is a collision on the top
+                    if (this.collidesTopLeft(GameObjects[i]) || this.collidesTop(GameObjects[i]) || this.collidesTopRight(GameObjects[i])) {
+                        this.setPosition(this.x, GameObjects[i].bottomEdge() + this.h / 2);
+
+                        // Transfer Momentum
+                        this.TransferMomentumY(GameObjects[i]);
+
+                        // If the active object and the target object are moving in opposite, reflect the target object
+                        if(this.yDir() === -GameObjects[i].yDir()){
+
+                            GameObjects[i].reflectY();
+
+                        }
+
+                        // Reflect the active objects
+                        this.reflectY();
+
+                        console.log("Top Collision");
+                    }
+
+                }
+            }
+        }
+
+        // DETECT COLLISION BOUNDARIES
+        // top edge boundary crossing prevention
+        if (this.y - (this.h / 2) < 0) {
+            this.y = this.h / 2;
+            this.reflectY();
+        }
+
+        // bottom edge boundary crossing prevention
+        else if (this.y + (this.h / 2) > height) {
+            this.y = height - (this.h / 2);
+            this.reflectY();
+        }
+
     };
 
 }
--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 // global variables and constants
 
-let canvas, Balls = [], paddleTop, paddleBottom, score, topScore = 0, bottomScore = 0, start = false;
+let canvas, Balls = [], GameObjects = [], paddleTop, paddleBottom, score, topScore = 0, bottomScore = 0, start = false;
 
 // ALL YOUR CONSTANTS ARE BELONG TO US. I DON'T KNOW WHAT WERE YELLING ABOUT
-const BALL_QUANTITY = 1, PADDLEACC = 10, CHAOS = 2, STAGE_WIDTH = 500, STAGE_HEIGHT = 500;
+const BALL_QUANTITY = 20, PADDLEACC = 10, CHAOS = 5, STAGE_WIDTH = 500, STAGE_HEIGHT = 500;
+const BALL_PUSH_SPEED = .5;
+const TRANSFER_COEFFICIENT = 1;
 
 // setup function --> deployed during page load
 function setup(){
     canvas = createCanvas(STAGE_WIDTH,STAGE_HEIGHT);
     centerCanvas();
 
+    paddleTop = new Paddle(0, width/2, 20, 0, 255, 0);
+    paddleBottom = new Paddle(1, width/2, height-20, 0, 255, 0);
+
+    GameObjects[0] = paddleTop;
+    GameObjects[1] = paddleBottom;
     for(let i=0;i<BALL_QUANTITY;i++) {
-        Balls.push(new Ball(width / 2, height / 2));
+        Balls.push(new Ball(2+i, width / 2, height / 2, i));
+        Balls[i].xvel = CHAOS*Math.random();
+        Balls[i].yvel = CHAOS*Math.random();
+        Balls[i].x = width*Math.random();
+        Balls[i].y = height*Math.random();
+        GameObjects[i+2] = Balls[i];
     }
 
-    paddleTop = new Paddle(width/2, 20, 0, 255, 0);
-    paddleBottom = new Paddle(width/2, height-20, 0, 255, 0);
     score = new Score();
+
+    //TESTING
+    Balls[0].setPosition(50,50);
+    Balls[1].xvel=1;
+    Balls[1].yvel=2;
 
 }
 
@@ -37,32 +52,38 @@ function draw(){
 
 // CONTROLS
     if(keyIsDown(LEFT_ARROW)){
-        paddleTop.accX(-PADDLEACC);
+        paddleBottom.accX(-PADDLEACC);
+        Balls[0].accX(-BALL_PUSH_SPEED);
     }
     else if(keyIsDown(RIGHT_ARROW)){
-        paddleTop.accX(PADDLEACC);
+        paddleBottom.accX(PADDLEACC);
+        Balls[0].accX(BALL_PUSH_SPEED);
+    }
+
+    if(keyIsDown(UP_ARROW)){
+        Balls[0].accY(-BALL_PUSH_SPEED);
+    }
+    else if(keyIsDown(DOWN_ARROW)){
+        Balls[0].accY(BALL_PUSH_SPEED);
     }
 
     if(keyIsDown(65)){
-        paddleBottom.accX(-PADDLEACC);
+        paddleTop.accX(-PADDLEACC);
     }
     else if(keyIsDown(68)){
-        paddleBottom.accX(PADDLEACC);
+        paddleTop.accX(PADDLEACC);
     }
 
-
 // UPDATES
+    paddleTop.update();
+    paddleBottom.update();
     for(let i=0;i<BALL_QUANTITY;i++) {
         Balls[i].update();
     }
-    paddleTop.update();
-    paddleBottom.update();
 
-// COLLISION DETECTECTION / BALL DEFLECTION
+// SCORE DETECTION
     for(let i=0;i<BALL_QUANTITY;i++) {
-        paddleTop.deflectBall(Balls[i]);
-        paddleBottom.deflectBall(Balls[i]);
-        score.scorePoint(Balls[i]);
+        // score.scorePoint(Balls[i]);
     }
 
 
